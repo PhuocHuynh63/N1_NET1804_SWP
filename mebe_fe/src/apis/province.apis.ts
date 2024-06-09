e4aa1330c612ff3d9b525510affef8ee77141d06
PhuocHuynh63 <phuochmse171830@fpt.edu.vn>
2024-06-09 15:23:46 +0700

    Login HTML

--- mebe_fe/src/apis/province.apis.ts
+++
@@ -1,9 +0,0 @@
-import provinceRequest from "~/constants/provinceRequest";
-
-
-const provinceApi = {
-  cityApi: () => provinceRequest.get('api/'),
-  districtApi: (city) => provinceRequest.get(`api/p/${city}?depth=2`),
-  wardApi: (district) => provinceRequest.get(`api/d/${district}?depth=2`),
-} 
-export default provinceApi;
--- mebe_fe/src/components/spinloading/spinText.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './styles.css';
-
-const SpinText = () => {
-  return <span className="spin-text"></span>;
-};
-
-export default SpinText;
