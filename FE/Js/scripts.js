961065cc57536e1611153ef1e6d0d8c06c2c0128
PhuocHuynh63 <phuochmse171830@fpt.edu.vn>
2024-06-11 21:53:26 +0700

    Header, Footer - React

--- FE/Js/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  window.addEventListener("scroll", function () {
-    if (window.scrollY > 50) {
-      document.querySelector("header").classList.add("scrolled");
-    } else {
-      document.querySelector("header").classList.remove("scrolled");
-    }
-  });
-});
