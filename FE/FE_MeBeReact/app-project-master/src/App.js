a543fbad4f33959e12e221d1e52ebb20bdb70dbf
PhuocHuynh63 <phuochmse171830@fpt.edu.vn>
2024-06-12 04:38:22 +0700

    Fix File

--- FE/FE_MeBeReact/app-project-master/src/App.js
+++ FE/FE_MeBeReact/app-project-master/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/homePage/HomePage';
 import Layout from './layout/Layout';
-import DetailPage from './pages/detailPage/ProductDetailPage';
+
 
 function App() {
   return (
@@ -10,7 +10,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout Component={HomePage} />} />
-          <Route path="/product/:productId" element={<Layout Component={DetailPage} />} />
           {/* <Route path="/" element={<About />} /> */}
           {/* <Route path="dashboard" element={<Dashboard />} /> */}
         </Routes>
