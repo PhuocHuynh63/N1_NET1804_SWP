5492525332ea07946acf7249b8f3f9f006a0a5e3
Phúc Hoàng <phucdlhse171826@fpt.edu.vn>
2024-06-12 03:49:48 +0700

    Upload categori fe

--- FE/FE_MeBeReact/app-project-master/src/App.js
+++ FE/FE_MeBeReact/app-project-master/src/App.js
@@ -1,9 +1,8 @@
-
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/homePage/HomePage';
 import Layout from './layout/Layout';
-import LoginPage from './pages/loginPage/LoginPage';
+import Category from './pages/categoryPage/Category';
 
 function App() {
   return (
@@ -11,6 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout Component={HomePage} />} />
+          <Route path="/category/:category" element={<Layout Component={Category} />} />
+       
           {/* <Route path="/" element={<About />} /> */}
           {/* <Route path="dashboard" element={<Dashboard />} /> */}
         </Routes>
